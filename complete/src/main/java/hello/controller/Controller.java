0fb4e3c1b63a64cc1cd6b4042b10ba6aeaf6f0e3
anastasiam <moiseevaanastasia.98@gmail.com>
2020-02-18 16:55:08 +0300

    add dockerfile

--- complete/src/main/java/hello/controller/Controller.java
+++ complete/src/main/java/hello/controller/Controller.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class Controller {
 
@@ -22,16 +20,16 @@ public class Controller {
     @Autowired
     private PredictionService predictionService;
 
-    /*@RequestMapping(value = "/rbk", method = RequestMethod.GET)
-    public Double getMaxDollarRate() {
-        return service.getMaxMonthDollarRate();
-    }*/
-
     @RequestMapping(value = "/rbk", method = RequestMethod.GET)
+    public String getMaxDollarRate() {
+        return "Max dollar rate: " + service.getMaxMonthDollarRate();
+    }
+
+    /*@RequestMapping(value = "/rbk", method = RequestMethod.GET)
     public String rbk() {
         List<Double> rateHistory = service.getRates();
         return "Dollar rate month ago: " + rateHistory.get(rateHistory.size() - 1);
-    }
+    }*/
 
     @RequestMapping(value = "/darkspy", method = RequestMethod.GET)
     public String weather() {
