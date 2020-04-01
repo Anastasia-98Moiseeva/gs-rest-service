c9d49c0752072cf83858bf5e5d6feb47d2c9b6b8
anastasiam <moiseevaanastasia.98@gmail.com>
2020-04-01 19:12:06 +0300

    move parameters to application properties file

--- complete/prediction/src/main/java/ru/sber/prediction/PredictionService.java
+++ complete/prediction/src/main/java/ru/sber/prediction/PredictionService.java
@@ -1,5 +1,6 @@
 package ru.sber.prediction;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +9,17 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public abstract class PredictionService {
 
+    @Value("${app.address.rbk}")
+    String rbkUrl;
+
+    @Value("${app.address.darksky}")
+    String darkskyUrl;
+
     public static void main(String[] args) {
         SpringApplication.run(PredictionService.class, args);
     }
 
     public Double predictWithMNK() {
-
-        String rbkUrl = "http://10.5.0.6:8081/rbk";
-        String darkskyUrl = "http://10.5.0.7:8082/darksky";
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> darkskyResponse = restTemplate.getForEntity(darkskyUrl, String.class);
